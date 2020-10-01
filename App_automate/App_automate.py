81bb56802dd4b020ad18ee409dd7ff4f58d5a19a
Alessandro Souza <sandrogenio@gmail.com>
2020-10-01 00:54:28 -0300

    visualização

---
+++ App_automate/App_automate.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import pyautogui
+import time
+
+
+
+os.chdir("C:\Desenvolvimento\RDS\RDS\RDS\RDS Encoder 703-720-730.exe")
+
+
+
+#pyautogui.moveTo(46,520,duration=2)
+#pyautogui.click()
+time.sleep(2)
+
