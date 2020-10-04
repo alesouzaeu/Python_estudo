1ad795e611824abe3e5fe88eeea8292a57a2003a
Alessandro Souza <sandrogenio@gmail.com>
2020-10-03 23:39:41 -0300

    Sqlite

---
+++ SQLite/aula1_sqlite.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+banco = sqlite3.connect('primeiro_banco.db')
+
+cursor = banco.cursor()
+
+# cursor.execute("CREATE TABLE pessoas (nome text, idade integer, email text)")
+
+
+cursor.execute("INSERT INTO pessoas VALUES('Alessandro', 29, 'sandrogenio@gmail.com')")
+
+banco.commit()
+
+
+""" cursor.execute("SELECT * FROM pessoas")
+print(cursor.fetchall()) """
+
+
